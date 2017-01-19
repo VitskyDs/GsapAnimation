3bca03344be730c78e1d9b1dd5151a55936a2cc2
issac <issac@showbox.com>
2017-01-19 15:57:52 +0200

    Finished Fashionista super animation

--- js/main.js
+++ js/main.js
@@ -75,16 +75,16 @@ tmln.addLabel("start", "0").addLabel("middle", "+0.9").addLabel("text", "+1.5").
     .fromTo(".fashionista-inner-rectangle-2", 0.5, {scale:0}, {scale: 1, ease: Back.easeOut.config(1.3)}, "start+=0.3")
     //retangles go left
         //rotate
-    .fromTo(superBg, 0.5,{rotation: 45}, {rotation: 0, ease:"rectangleLeft"}, "middle-=0.2")
-    .fromTo(".fashionista-rectangle", 0.5,{rotation: 45}, {rotation: -135, ease:"rectangleLeft"}, "middle-=0.2")
+    .fromTo(superBg, 0.5,{rotation: -45}, {rotation: 0, ease:"rectangleLeft"}, "middle-=0.2")
+    .fromTo(".fashionista-rectangle", 0.5,{rotation: 45}, {rotation: 135, ease:"rectangleLeft"}, "middle-=0.2")
         //go left
     .fromTo(superBg, 0.5,{x:255, scale:0.8}, {x:0, scale:1, ease:Circ.easeInOut}, "middle")
     .fromTo(".fashionista-rectangle", 0.5,{ x:320 , scale:0.5}, {x: 1, scale:1, ease:Circ.easeInOut}, "middle")
     //super enlarges
     .to(".super-headline, .super-subtitle",0.5, {className:"+=font-large", ease: Circ.easeInOut}, "-=0.3")
     //text enters
-    .fromTo(".super-headline", 0.5, {y:-200, x:25}, {y:0, ease:Power3.easeOut}, "text")
-    .fromTo(".super-subtitle", 0.5, {y:200, x:25}, {y:0, ease:Power3.easeOut}, "text")
+    .fromTo(".super-headline", 0.5, {y:-200, x:25}, {y:0, ease:Back.easeOut.config(1)}, "text")
+    .fromTo(".super-subtitle", 0.5, {y:200, x:25}, {y:0, ease:Back.easeOut.config(1.2)}, "text+=0.2")
     //reverse
     .addDelay(1)
     .to(superBg,0,{x:0, onComplete:reverse})
